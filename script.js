25aade0bac8ec45ac7e546aaf478929ba6978afa
Ziad As-Sahi <ziadelsahy72@gmail.com>
2025-09-07 14:07:59 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll إضافي بالـ JS (اختياري)
-document.querySelectorAll('a[href^="#"]').forEach(link=>{
-  link.addEventListener('click', e=>{
-    e.preventDefault();
-    document.querySelector(link.getAttribute('href'))
-      .scrollIntoView({behavior:'smooth'});
-  });
-});
