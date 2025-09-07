68750bbfb726e8ae01001ca2263b7c9e1e055546
Ziad As-Sahi <ziadelsahy72@gmail.com>
2025-09-07 14:07:29 +0300

    Update script.js

--- Task1/script.js
+++ Task1/script.js
@@ -1,1 +1,9 @@
+// Smooth scroll إضافي بالـ JS (اختياري)
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+  link.addEventListener('click', e=>{
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({behavior:'smooth'});
+  });
+});
 
