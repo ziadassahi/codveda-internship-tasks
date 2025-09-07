c02fbb5b5881ab781ee4229ece6f2c4049660c97
Ziad As-Sahi <ziadelsahy72@gmail.com>
2025-09-07 14:36:55 +0300

    Update script.js

--- Task3/script.js
+++ Task3/script.js
@@ -1,1 +1,27 @@
+const countDisplay = document.getElementById("count");
+const incrementBtn = document.querySelector(".increment");
+const decrementBtn = document.querySelector(".decrement");
+const resetBtn = document.querySelector(".reset");
+
+let count = 0;
+
+// Increment
+incrementBtn.addEventListener("click", () => {
+  count++;
+  countDisplay.textContent = count;
+});
+
+// Decrement (ما ينزلش تحت الصفر)
+decrementBtn.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+    countDisplay.textContent = count;
+  }
+});
+
+// Reset
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  countDisplay.textContent = count;
+});
 
